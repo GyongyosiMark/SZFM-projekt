4045a498452b4322e7af76662daf39b86387b95d
Kaiusz <55788794+Kaiusz@users.noreply.github.com>
2021-11-11 10:44:47 +0100

    sudden change of plans

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -1,112 +1,98 @@
-from kivy.app import App
-from kivy.graphics import Rectangle
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
-from kivy.clock import Clock
+import pygame
+from pygame.locals import *
 
-def collides(rect1, rect2):
-    # sima AABB utkozes detektor: ha a negyzetek fedik egymast akkor utkoznek
-    r1x = rect1[0][0]
-    r1y = rect1[0][1]
-    r2x = rect2[0][0]
-    r2y = rect2[0][1]
-    r1w = rect1[1][0]
-    r1h = rect1[1][1]
-    r2w = rect2[1][0]
-    r2h = rect2[1][1]
-    if r1x < r2x + r2w and r1x + r1w > r2x and r1y < r2y + r2h and r1y + r1h > r2y:
-        return True
-    else:
-        return False
+pygame.init()
 
+screen_width = 1000
+screen_height = 1000
 
-class PlatformerGame(Widget):
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+#define game variables
+tile_size = 50
 
-        self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
-        # keyboard objektum, ez altal veszunk be keyboard inputot
+#load images
+#sun_img = pygame.image.load('img/sun.png')
+bg_img = pygame.image.load('../animation/sky.png')
 
-        self._keyboard.bind(on_key_down=self._on_key_down)
-        # bindoljuk hozza azt, hogy ha van lenyomott billentyu csinaljon valamit
-        self._keyboard.bind(on_key_up=self._on_key_up)
-        # ha elengedjuk a billentyut
+def draw_grid():
+    for line in range(0, 20):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
-        with self.canvas:
-            # letrehozzuk a karakter, alakitjuk a kv fileba
-            self.player = Rectangle(source="../animation/player.png", size=(80,80), pos=(60, 60))
-            self.enemy = Rectangle(source="../animation/enemy.png", pos=(400, 100))
+class World():
+    def __init__(self, data):
+        self.tile_list = []
 
-        self.keysPressed = set()
-        # halmazban taroljuk az aktualisan lenyomott billentyuket
+        #load images
+        dirt_img = pygame.image.load('../animation/dirt.png')
+        grass_img = pygame.image.load('../animation/grass.png')
 
-        Clock.schedule_interval(self.move_step, 0)
-        # minden frameben tortenik valami (ezesetben a mozgas)
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if tile == 2:
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
 
-    def _on_keyboard_closed(self):
-        # ha nincs mar input akkor unbindoljuk
-        self._keyboard_unbind(on_key_down=self._on_key_down)
-        self._keyboard_unbind(on_key_up=self._on_key_up)
-        self._keyboard = None
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
 
-    def _on_key_down(self, keyboard, keycode, text, modifiers):
-        # mi tortenik ha lenyomjuk
-        self.keysPressed.add(text)
+world_data = [
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1],
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1],
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1],
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1],
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
-    def _on_key_up(self, keyboard, keycode):
-        # ha mar nem nyomjuk akkor elvesszuk a halmazbol
-        text = keycode[1]
-        if text in self.keysPressed:
-            self.keysPressed.remove(text)
+world = World(world_data)
 
-    isJump = False
-    jumpCount = 12
+run = True
+while run:
 
-    def move_step(self, dt):
-        # mostmar a lepesek egysegesek eszkoztol fuggetlenul(idohoz van kotve)
-        currentx = self.player.pos[0]
-        currenty = self.player.pos[1]
+    screen.blit(bg_img, (0, 0))
+    #screen.blit(sun_img, (100, 100))
 
-        step_size = 250 * dt
+    world.draw()
 
-        if 'a' in self.keysPressed and currentx > step_size:
-            currentx -= step_size
-        if 'd' in self.keysPressed and currentx < self.size[0] - self.player.size[0] - step_size:
-            currentx += step_size
-        if not self.isJump:
-            if 'w' in self.keysPressed:
-                self.isJump = True
-        else:
-            if self.jumpCount >= -12:
-                neg = 1
-                if self.jumpCount < 0:
-                    neg = -1
-                currenty += (self.jumpCount ** 2) * 0.2 * neg
-                self.jumpCount -= 1
-            else:
-                self.isJump = False
-                self.jumpCount = 12
-        #gravitacio
-        if (not self.isJump) and currenty > self.player.size[1] - step_size:
-            currenty -= 5
-        # delta ertek aminek segitsegevel meg az utkozes elott erzekeljuk azt
-        self.player.pos = (currentx, currenty)
-        delta = (currentx + step_size, currenty + step_size)
-        if collides((self.player.pos, self.player.size), (self.enemy.pos, self.enemy.size)):
-            print("Collides!")
-            currentx = delta[0]
-            currenty = delta[1]
-            self.player.pos = (currentx, currenty)
+    draw_grid()
 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-class PlatformerApp(App):
-    # példányosítunk egy alkalmazást ami örököl az ősosztályból
-    def build(self):
-        # ez a metódus építi meg az appunkat
-        return PlatformerGame()
+    pygame.display.update()
 
-
-if __name__ == "__main__":
-    # app futtatása
-    PlatformerApp().run()
+pygame.quit()
