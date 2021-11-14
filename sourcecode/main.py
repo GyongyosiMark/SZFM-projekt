c9aab147ad4fc93028ead6e7ef7c567aa3853e17
Stoky <61266396+stoky123@users.noreply.github.com>
2021-11-14 12:55:37 +0100

    Fixing database issue

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -1,6 +1,117 @@
 
 import pygame
 from pygame.locals import *
+import tkinter as tk
+import mysql.connector
+import getpass
+import os
+
+current_player = "guest"
+
+################################################################################################
+
+def init_database():
+    try:
+        database_username = os.environ["database_name"]
+        database_password = os.environ["database_password"]
+    except:
+        database_username = input("Database username: ")
+        database_password = getpass.getpass("Database password: ")
+
+    my_database = mysql.connector.connect(
+        host="localhost",
+        user=database_username,
+        passwd=database_password
+    )
+
+    my_cursor = my_database.cursor()
+    my_cursor.execute("CREATE DATABASE IF NOT EXISTS loginSystem;")
+
+    my_database = mysql.connector.connect(
+        host="localhost",
+        user=database_username,
+        passwd=database_password,
+        database="loginSystem"
+    )
+
+    my_cursor = my_database.cursor()
+
+    my_cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+        id MEDIUMINT NOT NULL AUTO_INCREMENT,
+        username VARCHAR(30) NOT NULL,
+        password VARCHAR(30) NOT NULL,
+        email VARCHAR(30) NOT NULL,
+        PRIMARY KEY (id, username),
+        UNIQUE (id),
+        UNIQUE (username),
+        UNIQUE(email)
+    );""")
+
+    return my_database, my_cursor
+
+try:
+    my_database, my_cursor = init_database()
+except mysql.connector.errors.ProgrammingError:
+    print("Invalid username or password.")
+    exit()
+
+window = tk.Tk()
+window.title('Hello Python')
+window.geometry("600x600")
+
+tk.Label(window, text="Username").pack()
+e1 = tk.Entry(window)
+e1.pack()
+tk.Label(window, text="Password").pack()
+e2 = tk.Entry(window, show="*")
+e2.pack()
+
+def login():
+    my_cursor.execute(f"SELECT password FROM users WHERE username = '{e1.get()}'")
+    result = my_cursor.fetchall()
+    if len(result) == 0:
+        print("This username does not exist.")
+    else:
+        if result[0][0] == e2.get():
+            current_player = e1.get()
+            print("Login successful")
+        else:
+            print("Invalid username or password")
+
+tk.Button(window, text="Login", command=login).pack()
+tk.Label(window, text="").pack()
+tk.Label(window, text="Username").pack()
+r1 = tk.Entry(window)
+r1.pack()
+tk.Label(window, text="Password").pack()
+r2 = tk.Entry(window, show="*")
+r2.pack()
+tk.Label(window, text="Email").pack()
+r3 = tk.Entry(window)
+r3.pack()
+
+def register():
+    try:
+        my_cursor.execute("SELECT MAX(id + 1) FROM users")
+        id = my_cursor.fetchall()[0][0]
+        if not id:
+            id = 1
+        my_cursor.execute(f"""
+        INSERT INTO users (id, username, password, email)
+        VALUES ({id}, '{r1.get()}', '{r2.get()}', '{r3.get()}')
+        """)
+        my_database.commit()
+        print("Successfully registered.")
+    except mysql.connector.errors.IntegrityError:
+        print("The username or email is already taken.")
+        my_database.rollback()
+
+tk.Button(window, text="Register", command=register).pack()
+tk.Label(window, text="").pack()
+tk.Button(window, text="Play", command=window.destroy).pack()
+window.mainloop()
+
+################################################################################################
 
 pygame.init()
 
