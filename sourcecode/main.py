83a683a01afe958bc357f3b465a0963feb369309
Kaiusz <55788794+Kaiusz@users.noreply.github.com>
2021-11-20 22:21:19 +0100

    ellenfelek + halal(animacioval)

--- sourcecode/main.py
+++ sourcecode/main.py
@@ -128,6 +128,7 @@ pygame.display.set_caption('Platformer')
 # a jatekbeli blokkok merete
 tile_size = 50
 main_manu = True
+game_over = 0
 
 # blokkok aniamacioja
 bg_img = pygame.image.load('../animation/sky.png')
@@ -177,6 +178,7 @@ class Player:
         #player animacio, pozicio, mozgas
         img = pygame.image.load('../animation/p1_stand.png')
         self.image = pygame.transform.scale(img, (40, 80))
+        self.dead_image = pygame.image.load("../animation/ghost.png")
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -188,60 +190,68 @@ class Player:
         self.direction = 1
         self.flip = False
 
-    def update(self):
+    def update(self, game_over):
         dx = 0
         dy = 0
+        if game_over == 0:
+            # input a billentyukrol
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
+                self.vel_y = -15
+                self.jumped = True
+            if not key[pygame.K_SPACE]:
+                self.jumped = False
+            if key[pygame.K_LEFT]:
+                dx -= 5
+                self.flip = True
+                self.direction = -1
+            if key[pygame.K_RIGHT]:
+                dx += 5
+                self.flip = False
+                self.direction = 1
 
-        # input a billentyukrol
-        key = pygame.key.get_pressed()
-        if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
-            self.vel_y = -15
-            self.jumped = True
-        if not key[pygame.K_SPACE]:
-            self.jumped = False
-        if key[pygame.K_LEFT]:
-            dx -= 5
-            self.flip = True
-            self.direction = -1
-        if key[pygame.K_RIGHT]:
-            dx += 5
-            self.flip = False
-            self.direction = 1
+            # gravitacio
+            self.vel_y += 1
+            if self.vel_y > 10:
+                self.vel_y = 10
+            dy += self.vel_y
 
-        # gravitacio
-        self.vel_y += 1
-        if self.vel_y > 10:
-            self.vel_y = 10
-        dy += self.vel_y
+            #utkozes TO DO
+            self.in_air = True
+            for tile in world.tile_list:
+                # vizsszintes utkozes
+                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                    dx = 0
+                # fuggoleges utkozes
+                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                    # alulrol utkozes blokkal, ugrasnal
+                    if self.vel_y < 0:
+                        dy = tile[1].bottom - self.rect.top
+                        self.vel_y = 0
+                    # felulrol utkozes blokkal, esesnel
+                    elif self.vel_y >= 0:
+                        dy = tile[1].top - self.rect.bottom
+                        self.vel_y = 0
+                        self.in_air = False
+            if pygame.sprite.spritecollide(self, enemy_group, False):
+                game_over = -1
 
-        #utkozes TO DO
-        self.in_air = True
-        for tile in world.tile_list:
-            # vizsszintes utkozes
-            if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
-                dx = 0
-            # fuggoleges utkozes
-            if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
-                # alulrol utkozes blokkal, ugrasnal
-                if self.vel_y < 0:
-                    dy = tile[1].bottom - self.rect.top
-                    self.vel_y = 0
-                # felulrol utkozes blokkal, esesnel
-                elif self.vel_y >= 0:
-                    dy = tile[1].top - self.rect.bottom
-                    self.vel_y = 0
-                    self.in_air = False
+            # player poziziojanak frissittese
+            self.rect.x += dx
+            self.rect.y += dy
 
-        # player poziziojanak frissittese
-        self.rect.x += dx
-        self.rect.y += dy
-
-        if self.rect.bottom > screen_height:
-            self.rect.bottom = screen_height
-            dy = 0
+            if self.rect.bottom > screen_height:
+                self.rect.bottom = screen_height
+                dy = 0
+        elif game_over == -1:
+            self.image = self.dead_image
+            if self.rect.y > 200:
+                self.rect.y -= 5
 
         # player rajzolasa a kepenyore
         screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+        return game_over
 
 #palya osztalya
 class World:
@@ -344,12 +354,13 @@ while run:
             run = False
         if highscore_button.draw() == True:
             pass
-    else:    
+    else:
         world.draw()
-        player.update()
-        enemy_group.draw(screen)
-        enemy_group.update()
+        if game_over == 0:
+            enemy_group.update()
 
+        enemy_group.draw(screen)
+        game_over = player.update(game_over)
 
         #draw_grid()
 
